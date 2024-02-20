dbf7fba64f97d4f99772bebf3d4f94afc3d508d5
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-02-20 12:28:31 +0100

    creating and using custom component + dynamic values

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -3,12 +3,32 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+// Words we will use to describe React
+const reactDescriptions = [
+  "Fundamentals",
+  "Crucial Concepts",
+  "Advanced Topics",
+];
+// random index for the reactDescriptions array
+function getRandomInt(maxValue) {
+  return Math.floor(Math.random() * (maxValue + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+  return (
+    <header>
+      <h1>First React</h1>
+      <p>This is the first react project!</p>
+      <p>{description} - This is a dynamic value</p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <h1>First React</h1>
-      </header>
+      <Header />
     </div>
   );
 }
